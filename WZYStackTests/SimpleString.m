2bb1c7cb7bcc80de54a5b65ca9c6dfa71c4036a8
Zeyu Wei <wzy1@meitu.com>
2016-05-23 18:09:24 +0800

    Comment out other tests

--- WZYStackTests/SimpleString.m
+++ WZYStackTests/SimpleString.m
@@ -1,26 +1,26 @@
+////
+////  SimpleString.m
+////  WZYStack
+////
+////  Created by Zeyu Wei on 5/23/16.
+////  Copyright © 2016 Hemster. All rights reserved.
+////
 //
-//  SimpleString.m
-//  WZYStack
+//#import <Kiwi/Kiwi.h>
 //
-//  Created by Zeyu Wei on 5/23/16.
-//  Copyright © 2016 Hemster. All rights reserved.
+//SPEC_BEGIN(SimpleStringSpec)
 //
-
-#import <Kiwi/Kiwi.h>
-
-SPEC_BEGIN(SimpleStringSpec)
-
-describe(@"SimpleString", ^{
-    context(@"when assigned to 'Hello world'", ^{
-        NSString *greeting = @"Hello world";
-        it(@"should exist", ^{
-            [[greeting shouldNot] beNil];
-        });
-        
-        it(@"should equal to 'Hello world'", ^{
-            [[greeting should] equal:@"Hello world"];
-        });
-    });
-});
-
-SPEC_END
+//describe(@"SimpleString", ^{
+//    context(@"when assigned to 'Hello world'", ^{
+//        NSString *greeting = @"Hello world";
+//        it(@"should exist", ^{
+//            [[greeting shouldNot] beNil];
+//        });
+//        
+//        it(@"should equal to 'Hello world'", ^{
+//            [[greeting should] equal:@"Hello world"];
+//        });
+//    });
+//});
+//
+//SPEC_END
