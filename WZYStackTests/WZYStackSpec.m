b30b0475622a51ee75a4b994b7ec62b935394922
Zeyu Wei <wzy1@meitu.com>
2016-05-23 11:26:43 +0800

    Add WZYStack Kiwi test example

---
+++ WZYStackTests/WZYStackSpec.m
@@ -0,0 +1,46 @@
+//
+//  WZYStackSpec.m
+//  WZYStack
+//
+//  Created by Zeyu Wei on 5/23/16.
+//  Copyright © 2016 Hemster. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+
+#import "WZYStack.h"
+
+
+SPEC_BEGIN(WZYStackSpec)
+
+describe(@"WZYStack", ^{
+    context(@"when created", ^{
+        __block WZYStack *stack = nil;
+        beforeEach(^{
+            stack = [WZYStack new];
+        });
+        
+        afterEach(^{
+            stack = nil;
+        });
+        
+        it(@"should have the class WZYStack", ^{
+            [[[WZYStack class] shouldNot] beNil];
+        });
+        
+        it(@"should exist", ^{
+            [[stack shouldNot] beNil];
+        });
+        
+        it(@"should be able to push and get top", ^{
+            [stack push:2.3];
+            [[theValue([stack top]) should] equal:theValue(2.3)];
+            
+            [stack push:4.6];
+            [[theValue([stack top]) should] equal:4.6 withDelta:0.001];
+        });
+        
+    });
+});
+
+SPEC_END
