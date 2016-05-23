9929130d502d9e9507af7e14121c0a20ab5941fd
Zeyu Wei <wzy1@meitu.com>
2016-05-23 11:26:15 +0800

    Add Simple Kiwi Example

---
+++ WZYStackTests/SimpleString.m
@@ -0,0 +1,26 @@
+//
+//  SimpleString.m
+//  WZYStack
+//
+//  Created by Zeyu Wei on 5/23/16.
+//  Copyright © 2016 Hemster. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+
+SPEC_BEGIN(SimpleStringSpec)
+
+describe(@"SimpleString", ^{
+    context(@"when assigned to 'Hello world'", ^{
+        NSString *greeting = @"Hello world";
+        it(@"should exist", ^{
+            [[greeting shouldNot] beNil];
+        });
+        
+        it(@"should equal to 'Hello world'", ^{
+            [[greeting should] equal:@"Hello world"];
+        });
+    });
+});
+
+SPEC_END
